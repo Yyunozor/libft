a37800a5339ce0e7960b325d5315134ce7458bf9
Yyunozor <yyunozor@icloud.com>
2024-09-17 22:36:00 +0200

    init

---
+++ ft_atoi.c
@@ -0,0 +1,30 @@
+#include <limits.h>
+
+int	ft_atoi(const char *str)
+{
+	int			i;
+	int			sign;
+	long long	result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + (str[i] - '0');
+		if (result * sign > INT_MAX)
+			return (INT_MAX);
+		if (result * sign < INT_MIN)
+			return (INT_MIN);
+		i++;
+	}
+	return ((int)(result * sign));
+}
---
+++ ft_isalnum.c
@@ -0,0 +1,8 @@
+int	isalnum(int c)
+{
+	if ((c >= '0' && c <= '9')
+		|| (c >= 'a' && c <= 'z')
+		|| (c >= 'A' && c <= 'Z'))
+		return (1);
+	return (0);
+}
