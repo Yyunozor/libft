e9088ea8ed2519e8653a0d4dbe43cb0fa3dbe9f3
Yyunozor <yyunozor@icloud.com>
2024-09-24 23:46:29 +0200

    add stuff

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,18 +6,17 @@
 /*   By: anpayot <anpayot@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/17 18:33:22 by anpayot           #+#    #+#             */
-/*   Updated: 2024/09/24 12:17:51 by anpayot          ###   ########.fr       */
+/*   Updated: 2024/09/24 23:45:18 by anpayot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stddef.h>
 
 size_t	ft_strlen(const char *s)
 {
 	size_t	len;
 
 	len = 0;
-	while (s[len])
+	while (*s++)
 		len++;
 	return (len);
 }
