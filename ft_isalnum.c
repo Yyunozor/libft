a3468dd13ff140dc0d1249a0a529c8ed42b2a4d0
Anthony Payot <yyunozor@icloud.com>
2024-10-01 18:22:25 +0200

    refactor

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -6,15 +6,11 @@
 /*   By: anpayot <anpayot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 16:20:23 by anpayot           #+#    #+#             */
-/*   Updated: 2024/10/01 16:21:14 by anpayot          ###   ########.fr       */
+/*   Updated: 2024/10/01 18:20:49 by anpayot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_isalnum(int c)
 {
-	if (ft_ilalpha(c) || ft_isdigit(c))
-	{
-		return (1);
-	}
-	return (0);
+	return (ft_isalpha(c) || ft_isdigit(c));
 }
