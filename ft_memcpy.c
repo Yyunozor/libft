b62fdc5564a56de6fda9a620b9a06a42c1849c5d
Anthony Payot <yyunozor@icloud.com>
2024-10-02 15:26:09 +0200

    add stuff

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -3,16 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: anpayot <anpayot@student.42.fr>            +#+  +:+       +#+        */
+/*   By: anpayot <anpayot@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 14:11:39 by anpayot           #+#    #+#             */
-/*   Updated: 2024/10/02 14:13:00 by anpayot          ###   ########.fr       */
+/*   Updated: 2024/10/02 15:17:58 by anpayot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*memcpy(void *restrict dst, const void *restrict src, size_t n)
+void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
 {
-	
+	unsigned char		*p_dest;
+	const unsigned char	*p_src;
+
+	p_dest = (unsigned char *)dst;
+	p_src = (const unsigned char *)src;
+	while (n--)
+		*p_dest++ = *p_src++;
+	return (dst);
 }
