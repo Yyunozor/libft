d90f99905ebd3e2b25788358f0f7a1804cb3118a
Yyunozor <yyunozor@icloud.com>
2024-10-04 22:31:58 +0200

    memmov and strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -1,37 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*   strlcpy.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: anpayot <anpayot@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/04 22:29:28 by anpayot           #+#    #+#             */
-/*   Updated: 2024/10/04 22:30:46 by anpayot          ###   ########.fr       */
+/*   Created: 2024/10/03 19:54:58 by anpayot           #+#    #+#             */
+/*   Updated: 2024/10/04 22:31:17 by anpayot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memmove(void *dst, const void *src, size_t len)
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
-	unsigned char		*d;
-	const unsigned char	*s;
-
-	d = (unsigned char *)dst;
-	s = (const unsigned char *)src;
-	if (dst == src || len == 0)
-		return (dst);
-	if (d > s)
-	{
-		d += len;
-		s += len;
-		while (len--)
-			*(--d) = *(--s);
-	}
-	else
-	{
-		while (len--)
-			*d++ = *s++;
-	}
-	return (dst);
+	
+	
+	return (ft_strlen(src));	
 }
