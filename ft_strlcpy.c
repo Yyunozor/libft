0d1d3016a1f62aedc88866fda781ff4c85a23e39
Yyunozor <yyunozor@icloud.com>
2024-10-04 22:34:09 +0200

    Your commit message

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   strlcpy.c                                          :+:      :+:    :+:   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: anpayot <anpayot@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 19:54:58 by anpayot           #+#    #+#             */
-/*   Updated: 2024/10/04 22:31:17 by anpayot          ###   ########.fr       */
+/*   Updated: 2024/10/04 22:34:01 by anpayot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
 	
-	
+		git pull origin main		git pull --no-rebase origin main		git pull --no-rebase origin main		git pull --ff-only origin main		git pull --no-rebase origin main		git add ft_strlcpy.c
+		git commit -m "Your commit message"
+		git pull --no-rebase origin main
 	return (ft_strlen(src));	
 }
