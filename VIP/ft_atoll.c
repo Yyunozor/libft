2a7b566c6b938907532ebf19805aceff5a9adde8
Yyunozor <yyunozor@icloud.com>
2024-10-08 10:11:05 +0200

    tree

---
+++ VIP/ft_atoll.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoll.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anpayot <anpayot@student.42lausanne.ch>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/17 19:30:22 by anpayot           #+#    #+#             */
+/*   Updated: 2024/10/03 13:52:32 by anpayot          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+long long	ft_lloverflow(int sign)
+{
+	if (sign == 1)
+		return (LLONG_MAX);
+	return (LLONG_MIN);
+}
+
+long	ft_atoll(const char *str)
+{
+	int		i;
+	int		sign;
+	long	result;
+
+	i = ft_skspace(str);
+	sign = 1;
+	result = 0;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (ft_isdigit(str[i]))
+	{
+		if (result > (LLONG_MAX - (str[i] - '0')) / 10)
+			return (ft_lloverflow(sign));
+		result = result * 10 + (str[i] - '0');
+		i++;
+	}
+	return (result * sign);
+}
---
+++ VIP/ft_memmove_xxl.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove_xxl.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anpayot <anpayot@student.42lausanne.ch>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/03 00:24:24 by anpayot           #+#    #+#             */
+/*   Updated: 2024/10/03 14:11:45 by anpayot          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static void	copy_forward(unsigned char *d, const unsigned char *s, size_t len)
+{
+	size_t	*d_word;
+	const size_t	*s_word;
+
+	if ((size_t)d % sizeof(size_t) == (size_t)s % sizeof(size_t))
+	{
+		d_word = (size_t *)d;
+		s_word = (const size_t *)s;
+		while (len >= sizeof(size_t))
+		{
+			*d_word++ = *s_word++;
+			len -= sizeof(size_t);
+		}
+		d = (unsigned char *)d_word;
+		s = (const unsigned char *)s_word;
+	}
+	while (len--)
+		*d++ = *s++;
+}
+
+static void	copy_backward(unsigned char *d, const unsigned char *s, size_t len)
+{
+	size_t	*d_word;
+	const size_t	*s_word;
+
+	if ((size_t)d % sizeof(size_t) == (size_t)s % sizeof(size_t))
+	{
+		d_word = (size_t *)(d + len);
+		s_word = (const size_t *)(s + len);
+		while (len >= sizeof(size_t))
+		{
+			len -= sizeof(size_t);
+			*--d_word = *--s_word;
+		}
+		d = (unsigned char *)d_word;
+		s = (const unsigned char *)s_word;
+	}
+	while (len--)
+		d[len] = s[len];
+}
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	if (dst == src || len == 0)
+		return (dst);
+	if (dst < src)
+		copy_forward((unsigned char *)dst, (const unsigned char *)src, len);
+	else
+		copy_backward((unsigned char *)dst, (const unsigned char *)src, len);
+	return (dst);
+}
