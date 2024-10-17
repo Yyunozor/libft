8d0f603f2c26f79e8a3089c485e7d857b92705ef
Yyunozor <yyunozor@icloud.com>
2024-10-17 02:45:31 +0200

    split

--- ft_split.c
+++ ft_split.c
@@ -6,20 +6,20 @@
 /*   By: anpayot <anpayot@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 18:24:43 by anpayot           #+#    #+#             */
-/*   Updated: 2024/10/16 22:47:51 by anpayot          ###   ########.fr       */
+/*   Updated: 2024/10/17 02:43:12 by anpayot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static void	ft_free_all(char **arr, char **ptr)
+static void	ft_free(char **arr, char **ptr)
 {
 	while (ptr-- > arr)
 		free(*ptr);
 	free(arr);
 }
 
-static size_t	ft_word_count(const char *s, char c)
+static size_t	ft_counter(const char *s, char c)
 {
 	size_t	count;
 
@@ -33,7 +33,7 @@ static size_t	ft_word_count(const char *s, char c)
 	return (count);
 }
 
-static char	*ft_get_next_word(const char **s, char c)
+static char	*ft_nxtwrd(const char **s, char c)
 {
 	const char	*start;
 
@@ -53,16 +53,16 @@ char	**ft_split(char const *s, char c)
 
 	if (!s)
 		return (NULL);
-	words = ft_word_count(s, c);
+	words = ft_counter(s, c);
 	res = (char **)malloc(sizeof(char *) * (words + 1));
 	if (!res)
 		return (NULL);
 	ptr = res;
 	while (words--)
 	{
-		*ptr = ft_get_next_word(&s, c);
+		*ptr = ft_nxtwrd(&s, c);
 		if (!*ptr++)
-			return (ft_free_all(res, ptr - 1), NULL);
+			return (ft_free(res, ptr - 1), NULL);
 	}
 	*ptr = NULL;
 	return (res);
